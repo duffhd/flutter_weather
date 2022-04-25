6e61a0cac6cbd670216cd059122b0636f6aa14c0
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-04-25 16:09:22 -0300

    replaced materialapp and routes for getmaterialapp and getPages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/util/routes.dart';
 import 'package:flutter_weather/view/home/home_screen.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        Routes.home: (context) => const HomeScreen(),
-      },
+    return GetMaterialApp(
+      getPages: [GetPage(name: Routes.home, page: () => const HomeScreen())],
       initialRoute: Routes.home,
       theme: ThemeData(
         primarySwatch: Colors.blue,
