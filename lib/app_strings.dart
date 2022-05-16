ed6e72fa43979edaa127cbc7df5a63aad130c10a
bluesden <7588320-bluesden@users.noreply.gitlab.com>
2022-05-16 09:15:17 -0300

    Add: app locale strings

---
+++ lib/app_strings.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class AppStrings extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    "en_US": {
+      "add_city": "Add you city",
+      "example_city": "London",
+      "search": "Search"
+    },
+    "pt_BR": {
+      "add_city": "Adicione sua cidade",
+      "example_city": "São Paulo",
+      "search": "Pesquisar"
+    }
+  };
+}
