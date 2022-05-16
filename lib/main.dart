06bbe0ddc4b261d43c8aad4942544f928e970102
bluesden <7588320-bluesden@users.noreply.gitlab.com>
2022-05-16 09:15:49 -0300

    Add: set app locale and fallback

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_weather/util/routes.dart';
 import 'package:flutter_weather/view/home/home_screen.dart';
 import 'package:get/get.dart';
 
+import 'app_strings.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,6 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      translations: AppStrings(),
+      locale: Get.deviceLocale,
+      fallbackLocale: const Locale("en", "US"),
       getPages: [GetPage(name: Routes.home, page: () => const HomeScreen())],
       initialRoute: Routes.home,
       theme: ThemeData(
